65b44c9b1a58607b50f2b0192da3580ae928932e
Matt Weston <MattOfNZ@users.noreply.github.com>
2023-12-02 17:15:19 +1300

    cute login screen

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { AI_NAME } from "@/features/theme/customise";
 import { signIn } from "next-auth/react";
-import { Avatar, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 import {
   Card,
@@ -16,14 +14,8 @@ export const LogIn = () => {
     <Card className="flex gap-2 flex-col min-w-[300px]">
       <CardHeader className="gap-2">
         <CardTitle className="text-2xl flex gap-2">
-          <Avatar className="h-8 w-8">
-            <AvatarImage src={"ai-icon.png"} />
-          </Avatar>
-          <span className="text-primary">{AI_NAME}</span>
+          <img src="/ai-icon.png" className="w-24 mx-auto" />
         </CardTitle>
-        <CardDescription>
-          Login in with your GitHub or Microsoft 365 account
-        </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
         <Button onClick={() => signIn("github")}>GitHub</Button>
