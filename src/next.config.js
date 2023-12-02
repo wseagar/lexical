b9c26534d1d8d0f4e7b4cc35013fe51127cf81ec
John Cave <me@johncave.co.nz>
2023-12-02 16:44:26 +1300

    Try hostname

--- src/next.config.js
+++ src/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   server: {
-    host: process.env.HOST || '127.0.0.1',
+    hostname: process.env.HOST || '127.0.0.1',
     port: process.env.PORT || 3000,
   },
 };
