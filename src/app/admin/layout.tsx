de97576b3b4611db21f33388c71eecacc9042cf8
Matt Weston <MattOfNZ@users.noreply.github.com>
2023-12-02 16:57:17 +1300

    Swap to submenu container

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,7 @@
 import { AdminMenu } from "@/features/admin/admin-menu/admin-menu";
 import { ChatMenu } from "@/features/chat/chat-menu/chat-menu";
-import { ChatMenuContainer } from "@/features/chat/chat-menu/chat-menu-container";
 import { MainMenu } from "@/features/main-menu/menu";
+import { SubmenuContainer } from "@/features/submenu/submenu-container";
 import { AI_NAME } from "@/features/theme/customise";
 
 export const dynamic = "force-dynamic";
@@ -20,9 +20,9 @@ export default async function RootLayout({
     <>
       <MainMenu />
       <div className="flex-1 flex rounded-md overflow-hidden bg-card/70">
-        <ChatMenuContainer>
+        <SubmenuContainer canClose={false}>
           <AdminMenu />
-        </ChatMenuContainer>
+        </SubmenuContainer>
         {children}
       </div>
     </>
