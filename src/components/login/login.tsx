6c0fee961620c719f32c286d907d2a1f805aff07
Matt Weston <MattOfNZ@users.noreply.github.com>
2023-12-02 17:01:22 +1300

    Local Dev

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -28,9 +28,7 @@ export const LogIn = () => {
       <CardContent className="grid gap-4">
         <Button onClick={() => signIn("github")}>GitHub</Button>
         <Button onClick={() => signIn("azure-ad")}> Microsoft 365</Button>
-        {process.env.NODE_ENV === "development" && (
-          <Button onClick={() => signIn("localdev")}>Basic Auth (DEV ONLY)</Button>
-        )}
+        <Button onClick={() => signIn("localdev")}>Local</Button>
       </CardContent>
     </Card>
   );
