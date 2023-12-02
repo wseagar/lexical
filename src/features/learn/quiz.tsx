db0dcd7637587abdbc89952f85d0348436424d26
Matt Weston <MattOfNZ@users.noreply.github.com>
2023-12-02 17:29:07 +1300

    Work in progress on quiz

--- src/features/learn/quiz.tsx
+++ src/features/learn/quiz.tsx
@@ -7,12 +7,11 @@ interface QuestionState {
   isCorrect: boolean | null;
 }
 
-const thisPage = learnData[0];
-
 export default function Home(props: any) {
-  //   const thisPage = learnData.find((item) => item.slug === props.slug ?? "");
+  const thisPage = learnData.find((item) => item.slug === props.slug ?? "");
 
   const [quizState, setQuizState] = useState<QuestionState[]>([]);
+  const [activeQuestion, setActiveQuestion] = useState<number>(-1);
 
   const updateQuizState = (
     index: number,
@@ -41,43 +40,65 @@ export default function Home(props: any) {
   return (
     <>
       {thisPage?.content}
-      {thisPage?.questions.map((question, questionIndex) => {
-        return (
-          <div
-            key={`question-${questionIndex}`}
-            className="flex flex-col space-y-1.5"
-          >
-            <div className="flex flex-col space-y-1">
-              <div className="flex flex-col space-y-0.5">
-                <span className="text-sm font-semibold">
-                  {question.question}
-                </span>
-              </div>
-              <div>
-                {question.options.map((option, index) => {
-                  return (
-                    <div
-                      key={`option-${index}`}
-                      className={optionClass(questionIndex, index)}
-                      onClick={() => {
-                        updateQuizState(questionIndex, index, option.correct);
-                      }}
-                    >
-                      {option.option}
-                    </div>
-                  );
-                })}
-              </div>
 
-              <div className="flex flex-col space-y-0.5"></div>
-            </div>
-            <div className="flex flex-col space-y-1">
-              <span className="text-sm font-semibold">Explanation</span>
-              <span className="text-xs">{question.explanation}</span>
-            </div>
-          </div>
-        );
-      })}
+      <div>
+        {activeQuestion === -1 && (
+          <>
+            <div onClick={() => setActiveQuestion(0)}>Start Quiz</div>
+          </>
+        )}
+
+        {activeQuestion > -1 && (
+          <>
+            {thisPage?.questions.map((question, questionIndex) => {
+              const thisQuestion = quizState[questionIndex];
+              return (
+                <div
+                  key={`question-${questionIndex}`}
+                  className="flex flex-col space-y-1.5"
+                >
+                  <div className="flex flex-col space-y-1">
+                    <div className="flex flex-col space-y-0.5">
+                      <span className="text-sm font-semibold">
+                        {question.question}
+                      </span>
+                    </div>
+                    <div>
+                      {question.options.map((option, index) => {
+                        return (
+                          <div
+                            key={`option-${index}`}
+                            className={optionClass(questionIndex, index)}
+                            onClick={() => {
+                              updateQuizState(
+                                questionIndex,
+                                index,
+                                option.correct
+                              );
+                            }}
+                          >
+                            {option.option}
+                          </div>
+                        );
+                      })}
+                    </div>
+
+                    <div className="flex flex-col space-y-0.5"></div>
+                  </div>
+                  {thisQuestion && (
+                    <div className="flex flex-col space-y-1">
+                      {thisQuestion.isCorrect && (
+                        <span className="text-xs">Correct!</span>
+                      )}
+                      <span className="text-xs">{question.explanation}</span>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </>
+        )}
+      </div>
 
       <pre>{JSON.stringify(quizState, null, 2)}</pre>
     </>
